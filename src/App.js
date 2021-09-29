341f52e456e61e01688cb98076556a25694a2d4c
yamilalopez <yam.lopez@hotmail.es>
2021-09-28 21:25:24 -0300

    hice navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
-import { Navbar } from 'react-bootstrap'
+import NavBar from './components/NavBar';
+
 
 function App() {
   return (
-    <div className="App">
-    <Navbar bg="dark">
-        <img src={logo} className="App-logo" alt="logo" />
-    </Navbar>
-    </div>
+    <> 
+      <NavBar />
+    </>
   );
 }
 
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,29 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css'
-import { Navbar } from 'react-bootstrap'
+import * as bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   return (
     <div className="App">
-    <Navbar bg="dark">
-    <Navbar.Brand>
-        <img src={logo} className="App-logo" alt="logo" />
-        Hawaii Deco
-    </Navbar.Brand>
+      <div>
+        <p>3 y 6 cuotas sin interés - Envíos a todo el país</p>
+      </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Hawaii Deco</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Productos
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Alfombras</a></li>
+            <li><a class="dropdown-item" href="#">Espejos</a></li>
+            <li><a class="dropdown-item" href="#">Velas aromáticas</a></li>
+            <li><a class="dropdown-item" href="#">Jarrones/Floreros</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Favoritos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contacto</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
-    <Nav>
-    <Nav.Link href="inicio">Inicio</Nav.Link>
-    <NavDropdown title="Productos">
-    <NavDropdown.Item href="">Alfombras</NavDropdown.Item>
-    <NavDropdown.Item href="">Espejos</NavDropdown.Item>
-    <NavDropdown.Item href="">Velas aromáticas</NavDropdown.Item>
-    <NavDropdown.Item href="">Floreros/Jarrones</NavDropdown.Item>
-    </NavDropdown>
-    <Nav.Link href="misFavoritos">Mis favoritos</Nav.Link>
-    <Nav.Link href="contactanos">Contactanos</Nav.Link>
-    </Nav>
-    </Navbar>
+
     </div>
   );
 }
