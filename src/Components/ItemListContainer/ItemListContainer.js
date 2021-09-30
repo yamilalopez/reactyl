d7f91a24ab451e446298b4243c05ae4ed63a1641
yamilalopez <yam.lopez@hotmail.es>
2021-09-30 12:29:32 -0300

    hice carpeta con itemlist

---
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ItemListContainer = ( {ProxProductos} ) => {
+    return (
+        <>
+          <h1 className="productos">{ProxProductos}</h1>  
+        </>
+    )
+}
+
+export default ItemListContainer
