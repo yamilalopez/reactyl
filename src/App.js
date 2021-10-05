c4eb7ae7f58da446ce3df1ab7808081d9ac7a661
yamilalopez <yam.lopez@hotmail.es>
2021-10-05 15:06:50 -0300

    cree carpeta Item con sus archivos para el tercer desafio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
-import NavBar from './components/NavBar';
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 
 function App() {
   return (
     <> 
       <NavBar />
+      <ItemListContainer ListaProductos="Sus productos aquí"/>
     </>
   );
 }
---
+++ src/Components/Item/Item.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import '../Item/Item.css';
+
+const Item = ({nombre , stock , precio , img , addToCartWidget}) => {
+
+    const [cantidad , setCantidad] = useState(0)
+
+    function add() {
+        if(cantidad <  stock) setCantidad(cantidad + 1)
+      }
+
+    function remove() {
+        if( cantidad > 1) setCantidad(cantidad - 1)
+      }
+
+    return (
+        <div class="row "> 
+        <div class="col-sm-6">
+          <div class="card">
+            <img src={img} class="card-img-top" alt="..."/>
+            <div class="card-body">
+              <h5 class="card-title">{nombre}</h5>
+              <p class="card-text">Funda 40x40</p>
+              <p class="card-text">${precio}</p>
+              <div className="">
+                    <button onClick={()=>remove()} className="btn col-xs-6 btn-dark botonMenos">-</button>
+                    <span>Cantidad : {cantidad} </span>
+                    <button onClick={()=>add()} className="btn col-xs-6 btn-dark botonMenos">+</button>
+                  </div>
+                  <div>
+                    <button onClick={()=>addToCartWidget(cantidad)} className="btn btn-dark botonAgregar">Agregar al Carrito</button>
+                  </div>
+                  <div>
+                    <p>Stock disponible: {stock}</p>
+                  </div>
+      </div>
+    </div>
+  </div>
+        </div>
+    )
+}
+
+export default Item
