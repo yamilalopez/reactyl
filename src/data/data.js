9da8ef0249f7ef57f7b52e30c70811818129d71e
yamilalopez <yam.lopez@hotmail.es>
2021-10-12 12:07:36 -0300

    cree data

---
+++ src/data/data.js
@@ -0,0 +1,17 @@
+import funda1 from '../img/funda1.jpg';
+import funda2 from '../img/funda2.jpg';
+import funda3 from '../img/funda3.jpg';
+import funda4 from '../img/funda4.jpg';
+import funda5 from '../img/funda5.jpg';
+import funda6 from '../img/funda6.jpg';
+
+
+
+export const productsList = [
+    {id:1 , nombre:"Funda Primitivo" , stock:7 , precio: 600 , img: funda1 , medidas: "40x40"},
+    {id:2 , nombre:"Funda Maldivas" , stock:8 , precio: 600 , img: funda2 , medidas: "40x40"},
+    {id:3 , nombre:"Funda Paris" , stock:7 , precio: 600 , img: funda3 , medidas: "40x40"},
+    {id:4 , nombre:"Funda Calamity" , stock:6 , precio: 600 , img: funda4 , medidas: "40x40"},
+    {id:5 , nombre:"Funda Lashes" , stock:9 , precio: 699 , img: funda5 , medidas: "50x30"},
+    {id:6 , nombre:"Funda Porter" , stock:4 , precio: 850 , img: funda6 , medidas: "45x45"},
+  ]
