7c7ae01eee8249e15251bde72a91f1d0c1867e64
yamilalopez <yam.lopez@hotmail.es>
2021-10-12 13:52:06 -0300

    cree el contenedor de item detail

---
+++ src/Components/ItemDetailContainer/ItemDetailContainer.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import { getProductById } from '../../helpers/getData.js';
+import ItemDetail from '../ItemDetail/ItemDetail';
+
+const ItemDetailContainer = ({ id }) => {
+
+    const [product, setProduct] = useState([]);
+
+    useEffect(() => {
+        getProductById(id, setProduct);
+        console.log(product);
+      }, []);
+
+    return (
+        <div>
+            <section>
+                {product ? <ItemDetail item={product} /> : <p>Obteniendo producto...</p>}
+            </section>   
+        </div>
+    )
+}
+
+export default ItemDetailContainer
