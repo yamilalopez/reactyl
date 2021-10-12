7e85b6b0fce6fdee2daa56c1bee282b5406b9802
yamilalopez <yam.lopez@hotmail.es>
2021-10-12 12:30:43 -0300

    cree el archivo del contador

---
+++ src/Components/ItemCount/ItemCount.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import '../Item/Item.css';
+
+
+const ItemCount = ({ remove , add , stock , addToCartWidget}) => {
+const [cantidad , setCantidad] = useState(1)
+
+    function add() {
+        if(cantidad <  stock) setCantidad(cantidad + 1)
+      }
+
+    function remove() {
+        if( cantidad > 1) setCantidad(cantidad - 1)
+      };
+
+      return (
+        <>
+        <div className="controls">
+            <button onClick={()=>remove()} className="btn botonCantidad">-</button>
+            <span>Cantidad : {cantidad} </span>
+            <button onClick={()=>add()} className="btn botonCantidad">+</button>
+      </div>
+      <div>
+            <button onClick={()=>addToCartWidget(cantidad)} className="btn  botonAgregar">Agregar al Carrito</button>
+      </div>
+      <div className="stock">
+        <p>Stock disponible: {stock}</p>
+      </div>
+      </>
+      );
+    };
+
+export default ItemCount;
