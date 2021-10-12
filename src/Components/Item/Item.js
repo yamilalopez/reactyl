87d15308c194977994066ca152e8555f3e16d170
yamilalopez <yam.lopez@hotmail.es>
2021-10-12 13:55:44 -0300

    modifique archivo

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -1,41 +1,20 @@
-import React, { useState } from 'react';
+import ItemCount from '../ItemCount/ItemCount';
 import '../Item/Item.css';
 
-const Item = ({nombre , stock , precio , img , medidas , addToCartWidget}) => {
-
-    const [cantidad , setCantidad] = useState(1)
-
-    function add() {
-        if(cantidad <  stock) setCantidad(cantidad + 1)
-      }
-
-    function remove() {
-        if( cantidad > 1) setCantidad(cantidad - 1)
-      }
-
+const Item = ({nombre , precio , img , medidas , stock}) => {
     return (
         <div class="row "> 
-        <div class="col-sm-6">
-          <div class="card">
-            <img src={img} class="card-img-top" alt="..."/>
-            <div class="card-body">
-              <h5 class="card-title">{nombre}</h5>
-              <p class="card-text">{medidas}</p>
-              <p class="card-text">${precio}</p>
-              <div className="controls">
-                    <button onClick={()=>remove()} className="btn botonCantidad">-</button>
-                    <span>Cantidad : {cantidad} </span>
-                    <button onClick={()=>add()} className="btn botonCantidad">+</button>
-                  </div>
-                  <div>
-                    <button onClick={()=>addToCartWidget(cantidad)} className="btn  botonAgregar">Agregar al Carrito</button>
-                  </div>
-                  <div className="stock">
-                    <p>Stock disponible: {stock}</p>
-                  </div>
-      </div>
-    </div>
-  </div>
+          <div class="col-sm-6">
+            <div class="card">
+              <img src={img} class="card-img-top" alt="..."/>
+              <div class="card-body">
+                <h5 class="card-title">{nombre}</h5>
+                <p class="card-text">{medidas}</p>
+                <p class="card-text">${precio}</p>  
+              </div>
+              <ItemCount stock={stock} />
+            </div>
+          </div>
         </div>
     )
 }
